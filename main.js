031e29e6fe19a85a93681554394653809482e064
lorenzo <brainstew927@gmail.com>
2019-03-30 22:57:09 +0100

    update 30 3

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 name_butt_pressable = true;
 
-file_name = "";
+let file_name = "";
 
-problemi = [];
+let problemi = [];
 
-soluzioni = [];
+let soluzioni = [];
+
+let text_comp = ["ciao", "mondo"]
+
+
+//genera il file lst
+function genera_file(){
+    var file = new Blob(text_comp, {type: "application/octet-stream"})
+
+    console.log("file generato: " + file)
+}
 
 function start(){
     console.log("started")
@@ -15,14 +25,14 @@ function start(){
 
     but_prob_s.disabled = true
     but_solu_s.disabled = true
-}
 
+    genera_file()
+}
 
 function runList(arg_1, arg_2){
     var select = document.getElementById('spinner_tip');
     select.options[select.options.length] = new Option(arg_1, arg_2);
 }
-
 
 function prob_press(){
     pro_txt = document.getElementById("tipo_prob_tx") 
@@ -32,17 +42,53 @@ function prob_press(){
     
     prob = pro_txt.value;
 
-    problemi.push(prob);
+    pro_txt.value = ""
+
+    if(prob!= ""){
+        problemi.push(prob);
+        
+        runList(prob, prob)
+
+        prob.value = ""
+
+        arr = []
+
+        soluzioni.push(arr)
     
-    runList(prob, prob)
+        console.log(soluzioni)
+        console.log(problemi)
 
-    prob.value = ""
-
-    console.log(problemi)
+        if(but_solu_s.disabled){
+            but_solu_s.disabled = false
+        }
+    }
+    else{
+        console.log("vuoto, non aggiunto all'array")
+    }
 }
 function solu_press(){
+    
     console.log("solu press!")
+    
     sol_txt = document.getElementById("solu_prob_tx")
+    spinn = document.getElementById("spinner_tip")
+    
+    soluzione = sol_txt.value
+    ind = spinn.selectedIndex
+    
+    if(soluzione != ""){
+
+        soluzioni[ind].push(soluzione)
+
+        sol_txt.value = ""
+
+        console.log("indice selez:  " + ind)
+        console.log(soluzioni)
+    }
+    else{
+        console.log("soluzione non aggiunta contenuto vuoto")
+    }
+
 
 }
 function name_press(){
@@ -60,6 +106,6 @@ function name_press(){
     console.log(file_name)
 
     but_prob_s.disabled = false
-    but_solu_s.disabled = false
+    //but_solu_s.disabled = false
 }
 
